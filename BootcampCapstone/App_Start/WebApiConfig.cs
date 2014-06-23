acca8253966979b7aa0ef9c4d51a083997463c87
nmoccia <nmoccia@asu.edu>
2014-06-23 12:31:51 -0400

    First commit to project

---
+++ BootcampCapstone/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BootcampCapstone
+{
+    public static class WebApiConfig
+    {
+        public static void Register (HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
