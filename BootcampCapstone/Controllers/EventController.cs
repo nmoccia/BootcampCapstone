f27688a3ca879789fc5dbea1f11bf78a6275d9c5
riley-pruitt <riley.pruitt@us.sogeti.com>
2014-06-24 08:14:51 -0400

    -Updated controllers and added UserQueries

---
+++ BootcampCapstone/Controllers/EventController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootcampCapstone.Controllers
+{
+    public class EventController : Controller
+    {
+        private EventManagerEntities db = new EventManagerEntities();
+
+        //
+        // GET: /Event/
+
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(e => e.Category).Include(e => e.Type);
+            return View(events.ToList());
+        }
+
+        //
+        // GET: /Event/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Event ev = db.Events.Find(id);
+            if (ev == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ev);
+        }
+
+        //
+        // GET: /Event/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "category1");
+            ViewBag.typeID = new SelectList(db.Types, "typeID", "type1");
+            return View();
+        }
+
+        //
+        // POST: /Event/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Event ev)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(ev);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "category1", ev.categoryID);
+            ViewBag.typeID = new SelectList(db.Types, "typeID", "type1", ev.typeID);
+            return View(ev);
+        }
+
+        //
+        // GET: /Event/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Event ev = db.Events.Find(id);
+            if (ev == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "category1", ev.categoryID);
+            ViewBag.typeID = new SelectList(db.Types, "typeID", "type1", ev.typeID);
+            return View(ev);
+        }
+
+        //
+        // POST: /Event/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Event ev)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ev).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "category1", ev.categoryID);
+            ViewBag.typeID = new SelectList(db.Types, "typeID", "type1", ev.typeID);
+            return View(ev);
+        }
+
+        //
+        // GET: /Event/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Event ev = db.Events.Find(id);
+            if (ev == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ev);
+        }
+
+        //
+        // POST: /Event/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event ev = db.Events.Find(id);
+            db.Events.Remove(ev);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BootcampCapstone/Queries/UserQueries.cs
@@ -0,0 +1,30 @@
+﻿using BootcampCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BootcampCapstone.Queries
+{
+    public class UserQueries 
+    {
+        public User Get(string name)
+        {
+            User user;
+            using (var db = new EventManagerEntities())
+            {
+                using (var tx = db.Database.Connection.BeginTransaction(IsolationLevel.Unspecified))
+                {
+                    user = db.Users.First(m => m.username == name);
+                    if (user == null)
+                    {
+                        return HttpNotFound();
+                    }
+                }
+            }
+        }
+
+    }
+}
