2d4f56a6e7b399a12a618a9951645f3dceb898bd
riley-pruitt <riley.pruitt@us.sogeti.com>
2014-06-23 21:44:02 -0400

    -Edited the index page

---
+++ BootcampCapstone/Models/AccountModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BootcampCapstone.Models
+{
+    public class AccountModel : Controller
+    {
+
+        public class RegisterModel
+        {
+            [Required]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public class LogInModel
+        {
+            [Required]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember Me")]
+            public bool RememberMe { get; set; }
+        }
+
+    }
+}
